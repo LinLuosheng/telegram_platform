8ae23bd2ddb81a4ad04030b3f3c88df802f728ad
LinLuosheng <482423614@qq.com>
2026-01-17 10:23:20 +0800

    fix: add missing import in C2TaskController

--- platform/backend/springboot-init-master/src/main/java/com/yupi/springbootinit/controller/C2TaskController.java
+++ platform/backend/springboot-init-master/src/main/java/com/yupi/springbootinit/controller/C2TaskController.java
@@ -22,6 +22,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import com.yupi.springbootinit.model.entity.C2Device;
